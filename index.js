5525d513b676803d5d94b3e77318a3fe939a5769
TranVanLam64 <vanlam8526@gmail.com>
2023-04-20 18:19:54 +0700

    api login logout register

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,8 @@ import {
 
 const router = express.Router();
 
-router.post("login", login);
-router.post("register", register);
-router.post("logout", logout);
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout);
 
 export default router;
