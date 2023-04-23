91c0e3dacd51df561f5d4a7fe6943060aa6cfdbb
TranVanLam64 <vanlam8526@gmail.com>
2023-04-23 17:31:56 +0700

    controller post, comment, like

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,13 @@
-export const getUser = (req, res) => {};
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+  const q = "select * from users where id=?";
+
+  db.query(q, [userId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    const { password, ...other } = data[0];
+    return res.json(other);
+  });
+};
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import likeRoutes from "./routes/likes.js";
 import commentRoutes from "./routes/comments.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
@@ -21,6 +22,26 @@ app.use(
   })
 );
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post(
+  "/api/upload",
+  upload.single("file"),
+  (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+  }
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
